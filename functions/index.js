a26276d195609bb7e085cd7aa21aa3febbfc29af
Gustavo Resende <62570230+resendegu@users.noreply.github.com>
2020-11-24 22:06:54 -0300

    alterações backend fracassos
    Tentei implementar de várias formas para que fosse retornado o valor do cloud functions, porém sem sucesso. Continuar tentando alternativas

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,3 +10,17 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
    response.send("Hello from Firebase!");
 });
 
+
+exports.verificadorDeAcesso = functions.https.onRequest((request, response) => {
+    let dados = JSON.parse(request.body)
+    return admin.database().ref(`sistemaEscolar/usuarios/${dados.email}/admin`).once('value')
+    .then(snapshot => {
+        if (snapshot.exists() && snapshot.val() == dados.uid) {
+            var data = JSON.stringify(snapshot.val())
+            console.log('oi')
+            response.status(200).send(snapshot.val())
+        } else {
+            response.status(403).send('Deu ruim')
+        }
+    })
+})
--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let user = usuarioAtual()
+var teste
+firebase.auth().onAuthStateChanged((user) => {
+    var dados = {email: user.email.replaceAll('@', '-').replaceAll('.', '_'), uid: user.uid}
+    fetch('http://localhost:5000/verificadorDeAcesso', {
+        body: JSON.stringify(dados),
+        method: 'POST',
+        mode: 'no-cors'
+    }).then(response => {
+       console.log(response)
+    }).catch(error => {
+        console.log(error)
+    })
     if (user != null) {
         // Carrega painel do usuário
-    } else if () {
-        
+        console.log('oi')
+    } else  {
+        console.log('oi')
     }
 })
+    
