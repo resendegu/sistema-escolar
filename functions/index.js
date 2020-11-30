962416153fe8f7415fa2f53fb60519bdab94c80d
Gustavo Resende <62570230+resendegu@users.noreply.github.com>
2020-11-30 15:01:33 -0300

    retornando

--- functions/index.js
+++ functions/index.js
@@ -4,23 +4,3 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-<<<<<<< HEAD
-
-exports.verificadorDeAcesso = functions.https.onCall((data, context) => {
-    const dados = data
-    console.log(context.auth)
-    let email = context.auth.token.email
-    email = email.replaceAll('@', '-').replaceAll('.', '_')
-    admin.database().ref(`sistemaEscolar/usuarios/${email}/admin`).once('value').then(value => {
-        if (value.exists() && value.val() == context.auth.uid) {
-            console.log(value.val())
-            let val = value.val()
-            return 'oi'
-        } else {
-            throw new functions.https.HttpsError('permission-denied', 'Você não possui permissão para acessar.')
-        }
-    })
-    
-})
-=======
->>>>>>> parent of a26276d... alterações backend fracassos
