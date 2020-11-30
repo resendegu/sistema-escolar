e05762a0ae6223249fe4d5b27d14bd5f06768e86
Gustavo Resende <62570230+resendegu@users.noreply.github.com>
2020-11-30 15:02:02 -0300

    retornando

--- functions/index.js
+++ functions/index.js
@@ -3,4 +3,11 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+<<<<<<< HEAD
 
+=======
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+>>>>>>> parent of 0a27f2b... alterei functions trabalho no login
