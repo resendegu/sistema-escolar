5b0c3e8cec1cbcae2557439fb891351b210dc874
Gustavo Resende <62570230+resendegu@users.noreply.github.com>
2020-12-05 12:03:05 -0300

    ajustes para iniciar a branch "secreteria"

--- functions/index.js
+++ functions/index.js
@@ -2,15 +2,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
 exports.verificadorDeAcesso = functions.https.onCall((data, context) => {
     try {
         if (context.auth.token.master == true) {
