844f76a65746fb74659b99568c410eb869a7458e
Gustavo Resende <gustavo@resende.app>
2021-06-14 22:07:48 -0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.986
+	let versao = 0.987
 	updatesRef.on('value', (snapshot) => {
 		let dados = snapshot.val()
 		if (versao < dados.versao) {
