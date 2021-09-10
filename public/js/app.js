229df8b86e17bd8fde25110ae6c5742b8d29bead
Gustavo Resende <gustavo@resende.app>
2021-09-09 21:26:19 -0300

    Versão 0.99902
    Descrição do novo Update:
    - Agora é possível visualizar uma lista de pré-matrículas na nova aba do sistema "Pré-matrículas".
    - Agora quando feita uma pré-matrícula, o e-mail pré-cadastrado receberá um aviso de que foi pré-matriculado com sucesso.
    Importância: Normal
    Data do lançamento: 2021-09-09

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.99901
+	let versao = 0.99902
 	updatesRef.on('value', (snapshot) => {
 		let dados = snapshot.val().lastUpdate
 
