2f3ae2dfe4d2f2ff7ba0bf94d71b826ac2a2d78f
Gustavo Resende <gustavo@resende.app>
2021-09-11 15:52:35 -0300

    Versão 0.99903
    Descrição do novo Update:
    - Agora é possível deletar uma pré matrícula.
    - Há uma botão para atualizar da lista de pré matrículas.
    - Também foi mudado uma configuração de banco de dados para que no ato do cadastro de alunos e pré matrículas, o sistema salve a data e hora e o usuário que fez a operação para consultas futuras.
    - Agora é possível editar os dados de pré-matrícula e visualizá-los na aba de Pré-Matrículas.
    - Agora é possível também matricular um aluno que já tem um pré cadastro.
    Importância: Importante
    Data do lançamento: 2021-09-11

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.99902
+	let versao = 0.99903
 	updatesRef.on('value', (snapshot) => {
 		let dados = snapshot.val().lastUpdate
 
