3987543d8d48bfea5f7f87ad9bf73bfaac3796c7
Gustavo Resende <gustavo@resende.app>
2021-10-02 21:12:55 -0300

    Versão 0.99925
    Descrição do novo Update:
    - Foi criado um espaço para que uma lista de chamados possa aparecer e também para serem abertos novos chamados na área da secretaria.
    - Assim que um usuário abre um chamado, os servidores remotos processam a abertura e abrem pedidos para envio dos e-mails para o suporte com cópia (Cc) para o solicitante.
    - Agora os alunos podem editar dados básicos por meio do portal do aluno.
    - Melhorias e correções gerais.
    Importância: Importante
    Data do lançamento: 2021-10-02

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.99922
+	let versao = 0.99925
 	updatesRef.on('value', (snapshot) => {
 		let dados = snapshot.val().lastUpdate
 
