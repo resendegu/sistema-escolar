abbf4206809a41b4e35e8573d1d21202a0d6ca0d
Gustavo Resende <gustavo@resende.app>
2021-11-09 21:23:04 -0300

    Versão 0.99939

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.99938
+	let versao = 0.99939
 	try {
 		updatesRef.on('value', (snapshot) => {
 			let dados = snapshot.val().lastUpdate
