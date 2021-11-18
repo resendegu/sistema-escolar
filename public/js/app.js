26eaceca85e139f30f936eff7e40135eff3abd8c
Gustavo Resende <gustavo@resende.app>
2021-11-18 20:39:16 -0300

    Versão 0.99943
    - Os balões flutuantes indicativos das funções dos ícones foi consertado.
    - Agora é possível excluir um plano cadastrado em um curso pela aba do Financeiro na área da ADM.
    - A tabela dos planos listados de cada curso foi melhorada, sendo atualizada automaticamente quando um novo plano é cadastrado ou excluído.

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged((usuario) => {
 })
 
 function update() {
-	let versao = 0.99942
+	let versao = 0.99943
 	try {
 		updatesRef.on('value', (snapshot) => {
 			let dados = snapshot.val().lastUpdate
